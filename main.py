ed974cdd5dc9984fac7fadf7e8b9195e60cb7f03
stevekb <stephenkb44@gmail.com>
2019-07-17 00:27:02 -0400

    main.py loads privatedata.json

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pyperclip
+import json
 
-skills = [
-    ("awesomeness","I learned how to be awesome while growing up."),
-    ("superness", "I'm super just like super man."),
-    ("communication", "I can speak English.")
-]
+with open("privateData.json", "r") as rf:
+    data = json.load(rf)
 
-# contact me if you are offering a software dev/engineering job.
-name = "Stephen Kelly-Bazan"
-phone = "xxx-xxx-xxxx"
-email = "xx@xx.com"
+skills = data['skills']
+name = data['name']
+phone = data['phone']
+email = data['email']
+
+# ask about cover letter info
 company = input("Company name? ") # first ask for the company name
 jobsite = input("What site did you find this job? ")
 jobname = input("What is the job name? ")
@@ -20,17 +20,17 @@ skill_3 = int(input("What block is skill 3 in? "))
 #after all seletions are made generate text and then print
 h1 = "Dear " + str(company) + " Human Resources:\n"
 p0 = ("Your job opportunity on " + str(jobsite) + " for a " + str(jobname)
-    + " interests me. With my experience in " + str(skills[skill_1][0]) + ", "
-    + str(skills[skill_2][0]) + " and " + str(skills[skill_3][0])
+    + " interests me. With my experience in " + str(skills[skill_1]['name']) + ", "
+    + str(skills[skill_2]['name']) + " and " + str(skills[skill_3]['name'])
     + ", I can serve " + str(company) + " well.\n")
-p1 = skills[skill_1][1] + "\n"
-p2 = skills[skill_2][1] + "\n"
-p3 = skills[skill_3][1] + "\n"
+p1 = skills[skill_1]['block'] + "\n"
+p2 = skills[skill_2]['block'] + "\n"
+p3 = skills[skill_3]['block'] + "\n"
 p4 = ("After you have read my resume for the details of my qualifications,"
     + " I will be happy to answer any questions. Please contact me at "
     + str(email) + ", so we can discuss how my experience in "
-    + str(skills[skill_1][0]) + ", " + str(skills[skill_2][0]) + " and "
-    + str(skills[skill_3][0]) + " will benefit you at " + str(company) +
+    + str(skills[skill_1]['name']) + ", " + str(skills[skill_2]['name']) + " and "
+    + str(skills[skill_3]['name']) + " will benefit you at " + str(company) +
     ". I look forward to hearing from you.\n\n")
 e1 = "Sincerely,\n" + str(name)
 
